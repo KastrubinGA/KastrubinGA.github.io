175d040370a0f3fe07483e8c034a6ed94b020b96
KastrubinGA <Kastrubing@mail.ru>
2024-11-24 14:57:48 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('github').addEventListener('click', function() {
+    window.open('https://github.com/yourusername', '_blank');
+});
+
+document.getElementById('linkedin').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/yourusername', '_blank');
+});
